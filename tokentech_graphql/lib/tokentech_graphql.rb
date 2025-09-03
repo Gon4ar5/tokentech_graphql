18c0791b8b28ad833009b4bad5dfecf8eece3045
Stanislav <s.gonchar@gltd.io>
2025-09-03 23:43:58 +0300

    require_relative

--- tokentech_graphql/lib/tokentech_graphql.rb
+++ tokentech_graphql/lib/tokentech_graphql.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "tokentech_graphql/version"
+require_relative "tokentech_graphql/support/id"
 
 module TokentechGraphql
   class Error < StandardError; end
-  # Your code goes here...
 end
